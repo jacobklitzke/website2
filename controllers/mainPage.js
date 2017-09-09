a1fc02de89a21c31f106920fec98a7bc890eebf9
Jacob Klitzke <jacobklitzke@gmail.com>
2017-09-08 23:52:25 -0400

    Completed contact section. Fixed fa bars

--- controllers/mainPage.js
+++ controllers/mainPage.js
@@ -9,4 +9,18 @@ app.controller('mainPageCtrl', ['$scope', '$mdSidenav', '$document',function($sc
         $mdSidenav('right').close();
         $document.scrollToElementAnimated(angular.element(document.getElementById(elementName)), 0, 2000);
     };
+
+    $scope.$watch(
+            function() { return $mdSidenav('right').isOpen(); },
+            function(newValue, oldValue) {
+                console.log(newValue);
+                if(newValue === false) {
+                    $scope.opened = false;
+                }
+                else {
+                    $scope.opened = true;
+                }
+            }
+        );
+
 }]);
